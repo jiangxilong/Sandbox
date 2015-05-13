c74eec665a0093984eb46a20d4783b5b76d8ee34
Huboter <rh.hubird@gmail.com>
2015-05-13 12:17:09 +0200

    shadow remove new

--- kineccv.cpp
+++ kineccv.cpp
@@ -113,49 +113,101 @@ void *cv_threadfunc (void *ptr) {
 }
 
 
+class myMutex {
+	public:
+		myMutex() {
+			pthread_mutex_init( &m_mutex, NULL );
+		}
+		void lock() {
+			pthread_mutex_lock( &m_mutex );
+		}
+		void unlock() {
+			pthread_mutex_unlock( &m_mutex );
+		}
+	private:
+		pthread_mutex_t m_mutex;
+};
+
+
+class MyFreenectDevice : public Freenect::FreenectDevice {
+	public:
+		MyFreenectDevice(freenect_context *_ctx, int _index)
+	 		: Freenect::FreenectDevice(_ctx, _index), m_buffer_depth(FREENECT_DEPTH_11BIT),
+			m_buffer_rgb(FREENECT_VIDEO_RGB), m_gamma(2048), m_new_rgb_frame(false),
+			m_new_depth_frame(false), depthMat(Size(640,480),CV_16UC1),
+			rgbMat(Size(640,480), CV_8UC3, Scalar(0)),
+			ownMat(Size(640,480),CV_8UC3,Scalar(0)) {
+
+			for( unsigned int i = 0 ; i < 2048 ; i++) {
+				float v = i/2048.0;
+				v = std::pow(v, 3)* 6;
+				m_gamma[i] = v*6*256;
+			}
+		}
+
+		// Do not call directly even in child
+		void VideoCallback(void* _rgb, uint32_t timestamp) {
+			std::cout << "RGB callback" << std::endl;
+			m_rgb_mutex.lock();
+			uint8_t* rgb = static_cast<uint8_t*>(_rgb);
+			rgbMat.data = rgb;
+			m_new_rgb_frame = true;
+			m_rgb_mutex.unlock();
+		};
+
+		// Do not call directly even in child
+		void DepthCallback(void* _depth, uint32_t timestamp) {
+			std::cout << "Depth callback" << std::endl;
+			m_depth_mutex.lock();
+			uint16_t* depth = static_cast<uint16_t*>(_depth);
+			depthMat.data = (uchar*) depth;
+			m_new_depth_frame = true;
+			m_depth_mutex.unlock();
+		}
+
+		bool getVideo(Mat& output) {
+			m_rgb_mutex.lock();
+			if(m_new_rgb_frame) {
+				cv::cvtColor(rgbMat, output, CV_RGB2BGR);
+				m_new_rgb_frame = false;
+				m_rgb_mutex.unlock();
+				return true;
+			} else {
+				m_rgb_mutex.unlock();
+				return false;
+			}
+		}
+
+		bool getDepth(Mat& output) {
+				m_depth_mutex.lock();
+				if(m_new_depth_frame) {
+					depthMat.copyTo(output);
+					m_new_depth_frame = false;
+					m_depth_mutex.unlock();
+					return true;
+				} else {
+					m_depth_mutex.unlock();
+					return false;
+				}
+			}
+	private:
+		std::vector<uint8_t> m_buffer_depth;
+		std::vector<uint8_t> m_buffer_rgb;
+		std::vector<uint16_t> m_gamma;
+		Mat depthMat;
+		Mat rgbMat;
+		Mat ownMat;
+		myMutex m_rgb_mutex;
+		myMutex m_depth_mutex;
+		bool m_new_rgb_frame;
+		bool m_new_depth_frame;
+};
+
+
 int main(int argc, char **argv)
 {
 
-        freenect_context *f_ctx;
-        freenect_device *f_dev;
-
-
-        int res = 0;
-        int die = 0;
-        printf("Kinect camera test\n");
-
-        if (freenect_init(&f_ctx, NULL) < 0) {
-                        printf("freenect_init() failed\n");
-                        return 1;
-                }
-
-        if (freenect_open_device(f_ctx, &f_dev, 0) < 0) {
-                printf("Could not open device\n");
-                return 1;
-        }
-
-        freenect_set_depth_callback(f_dev, depth_cb);
-        freenect_set_video_callback(f_dev, rgb_cb);
-        //freenect_video_format requested_format(FREENECT_VIDEO_RGB);
-
-        //create opencv display thread
-        res = pthread_create(&cv_thread, NULL, cv_threadfunc, (void*) depthimg);
-        if (res) {
-                printf("pthread_create failed\n");
-                return 1;
-        }
-
-        printf("init done\n");
-
-
-
-        freenect_start_depth(f_dev);
-        freenect_start_video(f_dev);
-
-        while(!die && freenect_process_events(f_ctx) >= 0 );
-
-
-
+        bool die(false);
 
         Mat depthMat(Size(640,480),CV_16UC1);
         Mat depthf  (Size(640,480),CV_8UC1);
@@ -163,14 +215,31 @@ int main(int argc, char **argv)
         Mat ownMat(Size(640,480),CV_8UC3,Scalar(0));
 
         Freenect::Freenect freenect;
-        MyFreenectDevice& f_dev = freenect.createDevice<MyFreenectDevice>(0);
+        MyFreenectDevice& device = freenect.createDevice<MyFreenectDevice>(0);
 
-        freenect_start_video(f_dev);
-        freenect_start_depth(f_dev);
+
+        //interpolation & inpainting
+        Mat _tmp,_tmp1; //minimum observed value is ~440. so shift a bit
+        Mat(depthMat - 400.0).convertTo(_tmp1,CV_64FC1);
+
+        Point minLoc; double minval,maxval;
+        minMaxLoc(_tmp1, &minval, &maxval, NULL, NULL);
+        _tmp1.convertTo(depthf, CV_8UC1, 255.0/maxval);  //linear interpolation
+
+                   //use a smaller version of the image
+        Mat small_depthf; resize(depthf,small_depthf,Size(),0.2,0.2);
+                    //inpaint only the "unknown" pixels
+        cv::inpaint(small_depthf,(small_depthf == 255),_tmp1,5.0,INPAINT_TELEA);
+
+        resize(_tmp1, _tmp, depthf.size());
+        _tmp.copyTo(depthf, (depthf == 255));  //add the original signal back over the inpaint
+
+        device.startVideo();
+        device.startDepth();
 
         while (!die) {
-            f_dev.getVideo(rgbMat);
-            f_dev.getDepth(depthMat);
+            device.getVideo(rgbMat);
+            device.getDepth(depthMat);
             depthMat.convertTo(depthf, CV_8UC1, 255.0/2048.0);
             cv::imshow("depth",depthf);
             char k = cvWaitKey(5);
@@ -179,27 +248,7 @@ int main(int argc, char **argv)
             }
         }
 
-        freenect_stop_video(f_dev);
-        freenect_stop_depth(f_dev);
-
-        //interpolation & inpainting
-
-        Mat _tmp,_tmp1; //minimum observed value is ~440. so shift a bit
-        Mat(depthMat - 400.0).convertTo(_tmp1,CV_64FC1);
-
-        Point minLoc; double minval,maxval;
-        minMaxLoc(_tmp1, &minval, &maxval, NULL, NULL);
-        _tmp1.convertTo(depthf, CV_8UC1, 255.0/maxval);  //linear interpolation
-
-        //use a smaller version of the image
-        Mat small_depthf; resize(depthf,small_depthf,Size(),0.2,0.2);
-        //inpaint only the "unknown" pixels
-        cv::inpaint(small_depthf,(small_depthf == 255),_tmp1,5.0,INPAINT_TELEA);
-
-        resize(_tmp1, _tmp, depthf.size());
-        _tmp.copyTo(depthf, (depthf == 255));  //add the original signal back over the inpaint
-
-
-
+        device.stopVideo();
+        device.stopDepth();
 
 }
