b45f42ef173510be66d28f5417c76abe47225940
Huboter <rh.hubird@gmail.com>
2015-05-20 13:01:56 +0200

    helligkeit verändert

--- kineccv.cpp
+++ kineccv.cpp
@@ -138,7 +138,7 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 			rgbMat(Size(640,480), CV_8UC3, Scalar(0)),
 			ownMat(Size(640,480),CV_8UC3,Scalar(0)) {
 
-			for( unsigned int i = 0 ; i < 2048 ; i++) {
+			for (unsigned int i = 0 ; i < 2048 ; i++) {
 				float v = i/2048.0;
 				v = std::pow(v, 3)* 6;
 				m_gamma[i] = v*6*256;
@@ -147,7 +147,7 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 
 		// Do not call directly even in child
 		void VideoCallback(void* _rgb, uint32_t timestamp) {
-			std::cout << "RGB callback" << std::endl;
+			//std::cout << "RGB callback" << std::endl;
 			m_rgb_mutex.lock();
 			uint8_t* rgb = static_cast<uint8_t*>(_rgb);
 			rgbMat.data = rgb;
@@ -157,7 +157,7 @@ class MyFreenectDevice : public Freenect::FreenectDevice {
 
 		// Do not call directly even in child
 		void DepthCallback(void* _depth, uint32_t timestamp) {
-			std::cout << "Depth callback" << std::endl;
+			//std::cout << "Depth callback" << std::endl;
 			m_depth_mutex.lock();
 			uint16_t* depth = static_cast<uint16_t*>(_depth);
 			depthMat.data = (uchar*) depth;
@@ -224,7 +224,7 @@ int main(int argc, char **argv) {
             device.getDepth(depthMat);
             //interpolation & inpainting
             Mat _tmp,_tmp1; //minimum observed value is ~440. so shift a bit
-            Mat(depthMat - 400.0).convertTo(_tmp1,CV_64FC1);
+            Mat(depthMat - 200.0).convertTo(_tmp1,CV_64FC1);
 
             Point minLoc; double minval,maxval;
             minMaxLoc(_tmp1, &minval, &maxval, NULL, NULL);
@@ -239,7 +239,7 @@ int main(int argc, char **argv) {
             _tmp.copyTo(depthf, (depthf == 255));  //add the original signal back over the inpaint
             cv::imshow("depth",depthf);
             char k = cvWaitKey(5);
-            if( k == 27 ){
+            if (k == 27) {
                 break;
             }
 
